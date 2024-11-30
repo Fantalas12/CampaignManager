0c3eefee9080f91f9945aa24757d173c276b198d
Fantalas12 <fantalas12@gmail.com>
2024-11-30 11:50:39 +0100

    Teszt2

--- CampaignManager.Persistence/Models/ErrorViewModel.cs
+++ CampaignManager.Persistence/Models/ErrorViewModel.cs
@@ -1,9 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace CampaignManager.Persistence.Models
 {
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string? ErrorMessage { get; set; }
+        public string? StackTrace { get; set; }
     }
 }
